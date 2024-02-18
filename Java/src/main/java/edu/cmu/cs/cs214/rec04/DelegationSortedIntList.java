2f6263df637ae056a478df001cce22d2eb420056
Niount <Nioount@gmail.com>
2024-02-18 16:24:20 -0500

    lab5

--- Java/src/main/java/edu/cmu/cs/cs214/rec04/DelegationSortedIntList.java
+++ Java/src/main/java/edu/cmu/cs/cs214/rec04/DelegationSortedIntList.java
@@ -10,7 +10,94 @@ package edu.cmu.cs.cs214.rec04;
  *
  */
 
-public class DelegationSortedIntList {
+public class DelegationSortedIntList implements IntegerList{
     // Write your implementation below with API documentation
+    private final SortedIntList sortedList = new SortedIntList();
+    private int totalAdded = 0;
 
+    /**
+     * Adds the specified int to the internal SortedIntList in sorted order.
+     * Increments the totalAdded counter if the list changes as a result.
+     *
+     * @param num an integer to be added to the list
+     * @return true if the list changed as a result of the call
+     */
+    @Override
+    public boolean add(int num) {
+        boolean added = sortedList.add(num);
+        if (added) {
+            totalAdded++;
+        }
+        return added;
+    }
+
+    /**
+     * Adds all of the elements of the specified IntegerList to the internal SortedIntList in sorted order.
+     * Increments the totalAdded counter based on the size of the list being added if the list changes.
+     *
+     * @param list IntegerList containing elements to be added to this list
+     * @return true if the list changed as a result of the call
+     */
+    @Override
+    public boolean addAll(IntegerList list) {
+        boolean changed = sortedList.addAll(list);
+        if (changed) {
+            totalAdded += list.size();
+        }
+        return changed;
+    }
+
+    /**
+     * Retrieves the integer at the specified position in this list.
+     *
+     * @param index index of the element to return
+     * @return the element at the specified position in this list
+     */
+    @Override
+    public int get(int index) {
+        return sortedList.get(index);
+    }
+
+    /**
+     * Removes the first occurrence of the specified element from the list,
+     * if it is present. Delegates to the sortedList instance.
+     *
+     * @param num an integer to be removed from the list, if present
+     * @return true if an element was removed as a result of this call
+     */
+    @Override
+    public boolean remove(int num) {
+        return sortedList.remove(num);
+    }
+
+    /**
+     * Removes from the list all of its elements that are contained in the
+     * specified IntegerList. Delegates to the sortedList instance.
+     *
+     * @param list IntegerList containing elements to be removed from this list
+     * @return true if the list changed as a result of the call
+     */
+    @Override
+    public boolean removeAll(IntegerList list) {
+        return sortedList.removeAll(list);
+    }
+
+    /**
+     * Returns the number of elements in this list. Delegates to the sortedList instance.
+     *
+     * @return number of elements in the list
+     */
+    @Override
+    public int size() {
+        return sortedList.size();
+    }
+
+    /**
+     * Returns the total number of attempted element insertions.
+     *
+     * @return the total number of elements attempted to be added
+     */
+    public int getTotalAdded() {
+        return totalAdded;
+    }
 }
--- Java/src/main/java/edu/cmu/cs/cs214/rec04/InheritanceSortedIntList.java
+++ Java/src/main/java/edu/cmu/cs/cs214/rec04/InheritanceSortedIntList.java
@@ -10,7 +10,50 @@ package edu.cmu.cs.cs214.rec04;
  *
  */
 
-public class InheritanceSortedIntList {
+public class InheritanceSortedIntList extends SortedIntList{
     // Write your implementation below with API documentation
+    private int totalAdded = 0;
 
+    /**
+     * Adds the specified int to the list in sorted order.
+     * Overrides the add method to increment the totalAdded counter.
+     *
+     * @param num an integer to be added to the list
+     * @return true if the list changed as a result of the call
+     */
+    @Override
+    public boolean add(int num) {
+        boolean added = super.add(num);
+        if (added) {
+            totalAdded++;
+        }
+        return added;
+    }
+
+
+    /**
+     * Adds all of the elements of the specified IntegerList to the list in sorted order.
+     * Overrides the addAll method to increment the totalAdded counter
+     * based on the size of the list being added.
+     *
+     * @param list IntegerList containing elements to be added to this list
+     * @return true if the list changed as a result of the call
+     */
+    @Override
+    public boolean addAll(IntegerList list) {
+        boolean changed = super.addAll(list);
+        if (changed) {
+            totalAdded += list.size();
+        }
+        return changed;
+    }
+
+    /**
+     * Returns the total number of attempted element insertions.
+     *
+     * @return the total number of elements attempted to be added
+     */
+    public int getTotalAdded() {
+        return totalAdded;
+    }
 }
